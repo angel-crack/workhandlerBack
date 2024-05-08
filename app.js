79fca58ea98e148976cbabad363e72de184e8329
AngelCrack <aeubarnes@gmail.com>
2024-05-07 22:55:13 -0500

    DELETE METHOD was added

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
   });
--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 let verifyToken = ( req, res, next) => {
     let token = req.get("Authorization");
-    console.log("Token para los casos: ", token)
     jwt.verify(token, process.env.TOKEN_SEED, (err, decoded) => {
         if(err){
             return res.status(401).json({
@@ -10,7 +9,6 @@ let verifyToken = ( req, res, next) => {
             });
         }
         req.usuario = decoded.user_id;
-        console.log(req.usuario)
         next()
     });
 };
